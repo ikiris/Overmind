9596ee788c5cae3070b2c058217e5bbf97e201aa
Blake Dunlap <ikiris@gmail.com>
2020-05-10 18:31:01 -0700

    safemode tweak

--- src/Overseer.ts
+++ src/Overseer.ts
@@ -527,7 +527,7 @@ export class Overseer implements IOverseer {
 											  colony.terminal]) as Structure[];
 		for (const structure of criticalStructures) {
 			if (structure.hits < structure.hitsMax &&
-				structure.pos.findInRange(colony.room.dangerousPlayerHostiles, 2).length > 0) {
+				structure.pos.findInRange(colony.room.dangerousPlayerHostiles, 3).length > 0) {
 				const ret = colony.controller.activateSafeMode();
 				if (ret != OK && !colony.controller.safeMode) {
 					if (colony.terminal) {
