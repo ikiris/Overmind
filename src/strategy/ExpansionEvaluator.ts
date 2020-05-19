f1b7d27cb381e2d8375f191db729d1e4092c8abf
Blake Dunlap <ikiris@gmail.com>
2020-05-19 13:25:21 -0700

    restrict expansions to same type of status as colony

--- src/strategy/ExpansionEvaluator.ts
+++ src/strategy/ExpansionEvaluator.ts
@@ -33,6 +33,7 @@ export class ExpansionEvaluator {
 		// This method is typed a little strangely to avoid some circular dependency problems
 
 		// This only gets run once per colony
+		const colStatus = RoomIntel.getRoomStatus(colonyRoomName)
 		if (_.keys(expansionData.possibleExpansions).length == 0 || Game.time > expansionData.expiration) {
 			// Generate a list of rooms which can possibly be settled in
 			const nearbyRooms = Cartographer.recursiveRoomSearch(colonyRoomName, MAX_EXPANSION_DISTANCE);
@@ -42,7 +43,7 @@ export class ExpansionEvaluator {
 				possibleExpansions = possibleExpansions.concat(nearbyRooms[depth]);
 			}
 			for (const roomName of possibleExpansions) {
-				if (Cartographer.roomType(roomName) == ROOMTYPE_CONTROLLER) {
+				if (Cartographer.roomType(roomName) == ROOMTYPE_CONTROLLER && RoomIntel.getRoomStatus(roomName) == colStatus) {
 					expansionData.possibleExpansions[roomName] = true;
 				}
 			}
