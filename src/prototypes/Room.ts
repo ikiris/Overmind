bfd72c0283f65a0a96bc3fb07218484a031aacd2
Blake Dunlap <ikiris@gmail.com>
2020-05-19 13:59:34 -0700

    Revert "cache avoidance test"
    This reverts commit d86ee9463912c68deff35ca85e15e1cce5346213.

--- src/prototypes/Room.ts
+++ src/prototypes/Room.ts
@@ -142,14 +142,7 @@ Object.defineProperty(Room.prototype, 'dangerousHostiles', {
 
 Object.defineProperty(Room.prototype, 'dangerousPlayerHostiles', {
 	get() {
-		if (this.threatLevel > 0.25) {
-			this._dangerousPlayerHostiles = this.find(FIND_HOSTILE_CREEPS, {filter: (creep: Creep) => creep.isHuman
-			&& !isAlly(creep.owner.username)
-			&& (creep.getActiveBodyparts(ATTACK) > 0
-																	 || creep.getActiveBodyparts(RANGED_ATTACK) > 0
-																	 || creep.getActiveBodyparts(WORK) > 0)
-			});
-		} else if (!this._dangerousPlayerHostiles) {
+		if (!this._dangerousPlayerHostiles) {
 			if (this.my) {
 				this._dangerousPlayerHostiles = _.filter(this.playerHostiles,
 												   (creep: Creep) => creep.getActiveBodyparts(ATTACK) > 0
