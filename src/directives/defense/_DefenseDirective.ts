f4dc31ee042aa91c1ec288b6b26e94ad429e2833
Blake Dunlap <ikiris@gmail.com>
2020-05-19 23:44:21 -0700

    test1

--- src/directives/defense/_DefenseDirective.ts
+++ src/directives/defense/_DefenseDirective.ts
@@ -6,6 +6,9 @@ import {Colony} from '../../Colony';
 
 export abstract class DefenseDirective extends Directive {
 
+	overlord: CombatOverlord;
+	overlords: {};
+
 	constructor(flag: Flag, colonyFilter?: (colony: Colony) => boolean) {
 		super(flag, colonyFilter);
 		(<Overseer>Overmind.overseer).combatPlanner.directives.push(this);
