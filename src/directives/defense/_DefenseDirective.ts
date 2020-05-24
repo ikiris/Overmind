1a9ea2697bd0f22878cca43d808fd1a52d6caf14
Blake Dunlap <ikiris@gmail.com>
2020-05-24 10:03:08 -0700

    Update _DefenseDirective.ts

--- src/directives/defense/_DefenseDirective.ts
+++ src/directives/defense/_DefenseDirective.ts
@@ -1,17 +1,17 @@
 import {CombatOverlord} from '../../overlords/CombatOverlord';
+import {Overlord} from '../../overlords/Overlord';
 import {Overseer} from '../../Overseer';
 import {Directive} from '../Directive';
 import {Colony} from '../../Colony';
 
 export abstract class DefenseDirective extends Directive {
 
-	overlord: CombatOverlord;
-	overlords: {};
-
 	constructor(flag: Flag, colonyFilter?: (colony: Colony) => boolean) {
 		super(flag, colonyFilter);
 		(<Overseer>Overmind.overseer).combatPlanner.directives.push(this);
 	}
 
-
+	get overlord(): CombatOverlord {
+		return (<CombatOverlord>_.filter(this.overlords, overlord => overlord instanceof CombatOverlord)[0]);
+	}
 }
