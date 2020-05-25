c17d34dd4677b68739b7bdd66e4c071e047622ff
Blake Dunlap <ikiris@gmail.com>
2020-05-24 18:37:05 -0700

    Update Encounter.ts

--- src/strategy/Encounter.ts
+++ src/strategy/Encounter.ts
@@ -1,3 +1,37 @@
 // High-level planning for skirmishing and combats
 
 import {log} from '../console/log';
+
+import {CombatIntel, CombatPotentials} from '../intel/CombatIntel';
+import {RoomIntel} from '../intel/RoomIntel';
+
+import {Graph} from '../algorithms/Graph'
+
+export class EncounterPlanner {
+	encounters: Encounter[];
+	roomName: string;
+	pos: RoomPosition;
+	room: Room | undefined;
+	creeps: CombatZerg[];
+
+	constructor() {
+		_.defaults(this.memory, defaultEncounterPlannerMemory);
+		this.creeps = [];
+	}
+
+	init() {
+		for (const encounter of this.encounters) {
+			this.refreshEncounter(encounter);
+		}
+	}
+}
+
+export class Encounter {
+	id: string;
+	graph: Graph;
+	creeps: CombatZerg[];
+	
+	init(): {
+		
+	}
+}
